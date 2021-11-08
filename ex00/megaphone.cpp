6af6d4721f17d10274e2713646bb606d915b195d
s-ike <shunt_i@yahoo.co.jp>
2021-11-08 14:55:56 +0900

    update ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: sikeda <sikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 09:08:10 by sikeda            #+#    #+#             */
-/*   Updated: 2021/11/08 09:17:56 by sikeda           ###   ########.fr       */
+/*   Updated: 2021/11/08 10:54:09 by sikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		for (int i = 1; i < argc; i++)
+		for (int i = 1; i < argc; ++i)
 		{
 			std::string	str = argv[i];
 			size_t		len = str.size();
 
-			for (size_t j = 0; j < len; j++)
+			for (size_t j = 0; j < len; ++j)
 			{
 				std::cout << static_cast<char>(toupper(str[j]));
 			}
